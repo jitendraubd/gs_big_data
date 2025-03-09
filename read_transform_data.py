8bd37f068c79e317d5df891f5705d5904d55b8c3
jitendraubd <jitupmca@gmail.com>
2025-03-09 11:27:41 +0530

    Update read_transform_data.py
    Update read_transform_data.py

--- read_transform_data.py
+++ read_transform_data.py
@@ -95,10 +95,11 @@ normalized_site_df = dim_possite_df.select(col("site_id"), col("site_label"), co
 normalize_subchannel_df = dim_possite_df.select(col("subchnl_id"), col("subchnl_label"), col("chnl_id")).distinct()
 normalize_channel_df = dim_possite_df.select(col("chnl_id"), col("chnl_label")).distinct()
 normalized_site_df.show()
-normalize_subsubstate_df = dim_pricestate_df.select(col("substate_id"), col("substate_label"), col("state_id")).distinct()
+normalize_substate_df = dim_pricestate_df.select(col("substate_id"), col("substate_label"), col("state_id")).distinct()
 normalize_state_df = dim_pricestate_df.select(col("state_id"), col("state_label")).distinct()
 
-normalized_prod_df = dim_prod_df.select(col("sku_id"), col("sku_label"), col("stylclr_id"), col("styl_id"), col("subcat_id"), col("dept_id"), col("issvc"), col("isasmbly"), col("isnfs")).distinct()
+normalized_prod_df = dim_prod_df.select(col("sku_id"), col("stylclr_id"), col("styl_id"), col("subcat_id"), col("dept_id"), col("issvc"), col("isasmbly"), col("isnfs")).distinct()
+normalized_sku_df = dim_prod_df.select(col("sku_id"), col("sku_label")).distinct()
 normalize_styleclr_df = dim_prod_df.select(col("stylclr_id"), col("stylclr_label")).distinct()
 normalize_style_df = dim_prod_df.select(col("styl_id"), col("styl_label")).distinct()
 normalize_subcategory_df = dim_prod_df.select(col("subcat_id"), col("subcat_label"), col("cat_id")).distinct()
@@ -112,3 +113,75 @@ normalize_fsclmth_df = dim_clnd_df.select(col("fsclmth_id"), col("fsclmth_label"
 normalize_fsclqrtr_df = dim_clnd_df.select(col("fsclqrtr_id"), col("fsclqrtr_label")).distinct()
 normalize_fsclyr_df = dim_clnd_df.select(col("fsclyr_id"), col("fsclyr_label")).distinct()
 normalize_ssn_df = dim_clnd_df.select(col("ssn_id"), col("ssn_label")).distinct()
+
+#staged fact table has foreign key  relationships with those tables
+# Broadcast all dimension tables during joins
+
+staged_fact_df = fact_transactions_df.alias("fact") \
+    .join(broadcast(normalized_site_df.alias("site")), 
+          col("fact.pos_site_id") == col("site.site_id"), "inner") \
+    .join(broadcast(normalized_sku_df.alias("sku")), 
+          col("fact.sku_id") == col("sku.sku_id"), "inner") \
+    .join(broadcast(normalized_clnd_df.alias("clnd")), 
+          col("fact.fscldt_id") == col(clnd.fscldt_id"), "inner") \
+    .join(broadcast(normalize_substate_df.alias("substate")), 
+          col("fact.price_substate_id") == col("substate.substate_id"), "inner") \
+    .select(
+        col("fact.order_id").alias("order_id"),
+        col("fact.line_id").alias("line_id"),
+        col("fact.pos_site_id").alias("pos_site_id"),
+        col("fact.sku_id").alias("sku_id"),
+        col("clnd.fsclwk_id").alias("fsclwk_id"),
+        col("fact.price_substate_id").alias("price_substate_id"),
+        col("fact.type").alias("type"),
+        col("fact.sales_units").alias("sales_units"),
+        col("fact.sales_dollars").alias("sales_dollars"),
+        col("discount_dollars").alias("discount_dollars")
+    )
+
+# Aggregation to create refined table 'mview_weekly_sales'
+mview_weekly_sales_df = staged_fact_df.groupBy("pos_site_id", "sku_id", "fsclwk_id", "price_substate_id", "type").agg(
+    sum("sales_units").alias("total_sales_units"),
+    sum("sales_dollars").alias("total_sales_dollars"),
+    sum("discount_dollars").alias("total_discount_dollars")
+)
+
+# Define SQL warehouse connection properties
+jdbc_url = "jdbc:databricks://<server-hostname>:443/default;transportMode=http;ssl=1;httpPath=<http-path>"
+connection_properties = {
+    "user": "<your-username>",
+    "password": "<your-password>",
+    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+}
+
+# Load existing data for incremental update
+existing_df = spark.read \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "mview_weekly_sales") \
+    .option("user", connection_properties["user"]) \
+    .option("password", connection_properties["password"]) \
+    .option("driver", connection_properties["driver"]) \
+    .load()
+
+# Perform incremental update by merging new and existing data
+incremental_df = existing_df.union(mview_weekly_sales_df) \
+    .groupBy("pos_site_id", "sku_id", "fsclwk_id", "price_substate_id", "type").agg(
+        sum("total_sales_units").alias("total_sales_units"),
+        sum("total_sales_dollars").alias("total_sales_dollars"),
+        sum("total_discount_dollars").alias("total_discount_dollars")
+    )
+
+# Write transformed data to SQL warehouse
+df_incremental.write \
+    .format("jdbc") \
+    .option("url", jdbc_url) \
+    .option("dbtable", "mview_weekly_sales") \
+    .option("user", connection_properties["user"]) \
+    .option("password", connection_properties["password"]) \
+    .option("driver", connection_properties["driver"]) \
+    .mode("overwrite") \
+    .save()
+
+print("Data successfully written to SQL warehouse")
+
